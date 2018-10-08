490e56c51fbe4828078c6203783c90217002939d
chun-lun Lin <lanexs777@gmail.com>
2018-10-08 13:38:05 -0700

    Create BM-IP2CIDR.c

---
+++ 18October/BM-IP2CIDR.c
@@ -0,0 +1,25 @@
+# 751
+class Solution {
+public:
+    vector<string> ipToCIDR(string ip, int n) {
+        vector<string> res;
+        long x=0;
+        stringstream ss(ip);
+        string t;
+        while(getline(ss, t, '.')){
+            x=x*256+stoi(t);
+        }
+        while(n>0){
+            long step=x & -x;
+            while(step>n) step/=2;
+            res.push_back(convert(x, step));
+            x += step;
+            n -= step;
+        }
+        return res;
+        
+    }
+    string convert(long x, int step) {
+        return to_string((x >> 24) & 255) + "." + to_string((x >> 16) & 255) + "." + to_string((x >> 8) & 255) + "." + to_string(x & 255) + "/" + to_string(32 - (int)log2(step));
+    }
+};
