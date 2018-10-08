4db09451ba4a3845c93d40d505fb11e826459768
chun-lun Lin <lanexs777@gmail.com>
2018-10-08 10:41:52 -0700

    Create AR-findCelebrity.c

---
+++ 18October/AR-findCelebrity.c
@@ -0,0 +1,17 @@
+# 277
+// Forward declaration of the knows API.
+bool knows(int a, int b);
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int res=0;
+        for(int i=0;i<n;i++) {
+            if(knows(res,i)) res=i;
+        }
+        for(int i=0;i<n;i++) {
+            if( i!=res && (knows(res,i) || !knows(i,res)) ) return -1;
+        }
+        return res;
+    }
+};
