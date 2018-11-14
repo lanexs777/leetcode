b94876d3c5c9e8d27ba9b0fa94ebce44050d36a9
chun-lun Lin <lanexs777@gmail.com>
2018-11-13 20:21:15 -0800

    Create AR-subArrSumEqualK.c

---
+++ 18November/AR-subArrSumEqualK.c
@@ -0,0 +1,16 @@
+# 560
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cum=0;
+        map<int, int> rec;
+        int cnt=0;
+        rec[0]=1;
+        for(int i=0;i<nums.size();i++) {
+            cum+=nums[i];
+            cnt+=rec[cum-k];
+            rec[cum]++;
+        }
+        return cnt;
+    }
+};
