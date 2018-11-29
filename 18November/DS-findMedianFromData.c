dd713b067a0805c2c0030e658df4d42d8a9b3d24
chun-lun Lin <lanexs777@gmail.com>
2018-11-28 18:25:40 -0800

    Create DS-findMedianFromData.c

---
+++ 18November/DS-findMedianFromData.c
@@ -0,0 +1,30 @@
+# 295
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        small.push(num);
+        large.push(-small.top());
+        small.pop();
+        if(small.size() < large.size()) {
+            small.push(-large.top());
+            large.pop();
+        }
+    }
+    
+    double findMedian() {
+        return small.size() > large.size() ? small.top(): 0.5*(small.top() -large.top());
+    }
+    priority_queue<long> small, large;
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
